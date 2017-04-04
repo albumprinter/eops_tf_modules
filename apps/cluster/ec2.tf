477c5410f0cef9281ed90dd6bb9dfb09bd76a453
Zoran Cikojevic <zcikojevic@vistaprint.com>
2017-04-04 15:47:48 +0200

    it wanted the name

--- apps/cluster/ec2.tf
+++ apps/cluster/ec2.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "app" {
   ami = "${var.ami}"
   instance_type = "${var.instance_type}"
-  availability_zone = "${var.availability_zone}"
   associate_public_ip_address = "${var.private == true ? false :true}"
-  iam_instance_profile = "${aws_iam_instance_profile.app.arn}"
+  iam_instance_profile = "${aws_iam_instance_profile.app.name}"
   security_groups = ["${aws_security_group.app.id}"]
   subnet_id = "${element(module.aws_core_data.private_subnets,count.index)}"
   vpc_security_group_ids = ["${aws_security_group.app.id}"]
@@ -59,4 +58,13 @@ resource "aws_security_group" "app" {
   name        = "SG-${var.app_name}"
   description = "Allow inbound traffic for ${var.app_name}"
   vpc_id      = "${module.aws_core_data.vpc_id}"
+  tags {
+    Name            = "${var.app_name}"
+    "Business Unit" = "${var.tags_business_unit}"
+    "Cost Center"   = "${var.tags_cost_center}"
+    Team            = "${var.tags_team}"
+    Purpose         = "${var.tags_purpose}"
+    Description     = "${var.description}"
+    Environment     = "${var.environment}"sdfaasdf
+  }
 }
