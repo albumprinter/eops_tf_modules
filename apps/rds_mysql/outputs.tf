f614bd24347d513fab0404206ae42671c62d24d8
zoran.cikojevic <zcikojevic@albelli.com>
2017-04-14 12:35:56 +0200

    merged latest master

--- apps/rds_mysql/outputs.tf
+++ apps/rds_mysql/outputs.tf
@@ -1,13 +1,13 @@
-output "db_endpoint" {
-  value = "${aws_db_instance.database.endpoint}"
-}
-
 output "db_id" {
   value = "${aws_db_instance.database.id}"
 }
 
 output "security_group_id" {
   value = "${aws_security_group.database.id}"
+}
+
+output "db_endpoint" {
+  value = "${aws_db_instance.database.endpoint}"
 }
 
 output "db_admin_username" {
