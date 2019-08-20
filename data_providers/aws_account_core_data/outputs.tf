39d6467b657b998afc30fade3497e6ecd2ae0a05
Alejandro Vallecilla <avallecillac@gmail.com>
2019-08-20 16:09:17 +0200

    Removes references to deleted datasource

--- data_providers/aws_account_core_data/outputs.tf
+++ data_providers/aws_account_core_data/outputs.tf
@@ -1,30 +1,30 @@
-output "vpc_id" {
-  value = "${data.terraform_remote_state.terraform-ap.vpc_id}"
-}
+# output "vpc_id" {
+#   value = "${data.terraform_remote_state.terraform-ap.vpc_id}"
+# }
 
-output "vpc_cidr" {
-  value = "${data.terraform_remote_state.terraform-ap.vpc_cidr}"
-}
+# output "vpc_cidr" {
+#   value = "${data.terraform_remote_state.terraform-ap.vpc_cidr}"
+# }
 
-output "availability_zones" {
-  value = "${data.aws_availability_zones.available.names}"
-}
+# output "availability_zones" {
+#   value = "${data.aws_availability_zones.available.names}"
+# }
 
-output "public_subnets" {
-  value = "${data.terraform_remote_state.terraform-ap.public_subnets}"
-}
+# output "public_subnets" {
+#   value = "${data.terraform_remote_state.terraform-ap.public_subnets}"
+# }
 
-output "private_subnets" {
-  value = "${data.terraform_remote_state.terraform-ap.private_subnets}"
-}
+# output "private_subnets" {
+#   value = "${data.terraform_remote_state.terraform-ap.private_subnets}"
+# }
 
-output "account_id" {
-  value = "${data.aws_caller_identity.current.account_id}"
-}
-output "tags_environment" {
-  value = "${var.environment}"
-}
+# output "account_id" {
+#   value = "${data.aws_caller_identity.current.account_id}"
+# }
+# output "tags_environment" {
+#   value = "${var.environment}"
+# }
 
-output "tags_type" {
-  value = "${var.type}"
-}
+# output "tags_type" {
+#   value = "${var.type}"
+# }
