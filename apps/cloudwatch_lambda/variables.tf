439c78158a9f4a7e3d1abf877529a0aefcad3e4b
Phil Jory <Philip.Jory@albelli.com>
2020-06-24 17:35:56 +0200

    Enable an alarm for duration & errors (for lambdas without an error queue)

--- apps/cloudwatch_lambda/variables.tf
+++ apps/cloudwatch_lambda/variables.tf
@@ -20,6 +20,7 @@ variable "period" {
 }
 variable "alarm_threshold" {}
 variable "alarm_action_arn" {}
+variable "alarm_timeout" {}
 variable "retention_days" {
   default = "30"
 }
@@ -32,3 +33,9 @@ variable "alarm_action_enabled" {
   default = "true"
 }
 variable "enable_cloudwatch_alarms" {}
+variable "enable_cloudwatch_error_alarm" {
+  default = 1
+}
+locals {
+  enable_error_alarm = "${var.enable_cloudwatch_alarms == 1 ? var.enable_cloudwatch_error_alarm : var.enable_cloudwatch_alarms}"
+}
