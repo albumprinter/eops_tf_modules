3a0360dc9d2d5d97efc4b277abe1337dc0c314c6
Martijn Mol <Martijn.Mol@albelli.com>
2023-05-04 09:49:42 +0200

    compatible with terraform 0.12 part 7

--- apps/api_gateway_resource/api_gateway_integration.tf
+++ apps/api_gateway_resource/api_gateway_integration.tf
@@ -1,7 +1,7 @@
 resource "aws_api_gateway_integration" "integration" {
   http_method             = "${aws_api_gateway_method.method[0].http_method}"
   rest_api_id             = "${var.api_gateway_rest_api_id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  resource_id             = "${aws_api_gateway_resource.resource[0].id}"
   type                    = "${var.api_gateway_integration_type}"
   integration_http_method = "${var.api_gateway_integration_method}"
   uri                     = "${var.api_gateway_integration_uri}"
