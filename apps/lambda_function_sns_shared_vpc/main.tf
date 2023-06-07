d802205ebbd455deed77110cf62c36748ef80019
mbbilgen <mbilgen@albelli.net>
2023-06-07 10:52:36 +0200

    add vpc module and update version

--- apps/lambda_function_sns_shared_vpc/main.tf
+++ apps/lambda_function_sns_shared_vpc/main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "1.73.0"
+}
+
 module "aws_shared_data" {
   source = "../../data_providers/shared-vpc-data"
   account_type = "${var.account_type}"
