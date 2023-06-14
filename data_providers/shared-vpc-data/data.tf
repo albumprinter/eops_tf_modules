12c6436a12725a7fd1acf1af25927f2948a31889
prajishajayaprakasan <prajisha.jayaprakasan@albelli.com>
2023-06-14 16:55:08 +0200

    change aws_subnet

--- data_providers/shared-vpc-data/data.tf
+++ data_providers/shared-vpc-data/data.tf
@@ -5,31 +5,40 @@ data "aws_vpc" "working_vpc" {
 }
 
 data "aws_subnets" "all" {
-  vpc_id = "${data.aws_vpc.working_vpc.id}"
+   filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.working_vpc.id]
+  }
 }
 
 data "aws_subnets" "public" {
-  vpc_id = "${data.aws_vpc.working_vpc.id}"
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.working_vpc.id]
+  }
   tags = {
     Tier = "public-*"
   }
 }
 
 data "aws_subnets" "private" {
-  vpc_id = "${data.aws_vpc.working_vpc.id}"
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.working_vpc.id]
+  }
   tags = {
     Tier = "private-*"
   }
 }
 
 data "aws_subnet" "public" {
-  count = "${length(data.aws_subnets.public.ids)}"
-  id    = "${data.aws_subnets.public.ids[count.index]}"
+  for_each = toset(data.aws_subnets.public.ids)
+  id       = each.value
 }
 
 data "aws_subnet" "private" {
-  count = "${length(data.aws_subnets.private.ids)}"
-  id    = "${data.aws_subnets.private.ids[count.index]}"
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
 }
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
