602e1f4a81f267c45fe7122e03aa51604d4a6cd0
prajishajayaprakasan <prajisha.jayaprakasan@albelli.com>
2023-06-14 15:02:31 +0200

    change into subnetids

--- data_providers/shared-vpc-data/data.tf
+++ data_providers/shared-vpc-data/data.tf
@@ -4,41 +4,34 @@ data "aws_vpc" "working_vpc" {
   }
 }
 
-data "aws_subnets" "all" {
-  filter {
-    name = "vpc_id"
-    values = [data.aws_vpc.working_vpc.id]
-  }
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.working_vpc.id}"
 }
 
-data "aws_subnets" "public" {
-   filter {
-    name = "vpc_id"
-    values = [data.aws_vpc.working_vpc.id]
-  }
+data "aws_subnet_ids" "public" {
+  vpc_id = "${data.aws_vpc.working_vpc.id}"
+
   tags = {
     Name = "public-*"
   }
 }
 
-data "aws_subnets" "private" {
- filter {
-    name = "vpc_id"
-    values = [data.aws_vpc.working_vpc.id]
-  }
+data "aws_subnet_ids" "private" {
+  vpc_id = "${data.aws_vpc.working_vpc.id}"
+
   tags = {
     Name = "private-*"
   }
 }
 
 data "aws_subnet" "public" {
-  count = "${length(data.aws_subnets.public.ids)}"
-  id    = "${data.aws_subnets.public.ids[count.index]}"
+  count = "${length(data.aws_subnet_ids.public.ids)}"
+  id    = "${tolist(data.aws_subnet_ids.public.ids)[count.index]}"
 }
 
 data "aws_subnet" "private" {
-  count = "${length(data.aws_subnets.private.ids)}"
-  id    = "${data.aws_subnets.private.ids[count.index]}"
+  count = "${length(data.aws_subnet_ids.private.ids)}"
+  id    = "${tolist(data.aws_subnet_ids.private.ids)[count.index]}"
 }
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
