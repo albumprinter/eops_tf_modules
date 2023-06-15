1d4754379b5b2b269ad5842df98f48d819dbe3fc
prajishajayaprakasan <prajisha.jayaprakasan@albelli.com>
2023-06-15 13:11:06 +0200

    check private

--- apps/lambda_function_api_gateway_all_methods_passthrough/lambda.tf
+++ apps/lambda_function_api_gateway_all_methods_passthrough/lambda.tf
@@ -19,7 +19,7 @@ resource "aws_lambda_function" "app" {
   memory_size = "${var.memory_size}"
   timeout = "${var.timeout}"
   vpc_config {
-    subnet_ids = flatten([module.aws_core_data.private_subnets])
+    subnet_ids = flatten([var.private])
     //subnet_ids = flatten([var.private == 1 ?   module.aws_core_data.private_subnets :  concat(module.aws_core_data.private_subnets,module.aws_core_data.public_subnets)])
     security_group_ids = ["${aws_security_group.sg_for_app.id}"]
   }
