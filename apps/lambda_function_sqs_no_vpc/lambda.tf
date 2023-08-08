63a1ddc0a715659310787cf9bb5d3824a56026f6
mbbilgen <mbilgen@albelli.net>
2023-08-08 13:53:39 +0200

    fix dimension error and fix count set error

--- apps/lambda_function_sqs_no_vpc/lambda.tf
+++ apps/lambda_function_sqs_no_vpc/lambda.tf
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "app" {
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   batch_size       = 1
   event_source_arn = "${aws_sqs_queue.sqs_queue.arn}"
-  function_name    = "${aws_lambda_function.app.arn}"
+  function_name    = "${aws_lambda_function.app[0].arn}"
   enabled          = "${var.event_source_mapping_active}"
 }
 
