7ddf1ca7aec7f3a5a816feb4f565ad2c181421f7
Daniel Betancur <danibeca@okazo.co>
2023-08-11 09:50:44 +0200

    Removing space

--- apps/lambda_function_scheduled/cloudwatch.tf
+++ apps/lambda_function_scheduled/cloudwatch.tf
@@ -2,8 +2,7 @@ resource "aws_cloudwatch_event_rule" "app" {
   name                = "${var.app_name}"
   schedule_expression = "${var.schedule_expression}"
   count               = "${var.enabled}"
-  tags = "${local.tags}"
-  
+  tags = "${local.tags}"  
   is_enabled = "${var.cloudwatch_event_rule_is_enabled}"
 }
 
